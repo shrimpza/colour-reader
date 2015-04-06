12513892612889e0437a6349cb51f958e1eb9e73
shrimpza <github@shrimpworks.za.net>
2015-04-06 11:19:27 +0200

    make ColourReader a more builder-style thing to allow customisation of parameters

--- src/net/shrimpworks/colours/ColourReader.java
+++ src/net/shrimpworks/colours/ColourReader.java
@@ -3,6 +3,7 @@ package net.shrimpworks.colours;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +13,43 @@ public class ColourReader {
 
 	private static final Hue[] HUES = new Hue[] { Hue.RED, Hue.YELLOW, Hue.GREEN, Hue.CYAN, Hue.BLUE, Hue.MAGENTA };
 
-	public static HSBColour averageColour(BufferedImage image, float resolution) {
+	private final List<Hue> hues;
+	private final float resolution;
+	private final float blackThreshold;
+	private final float whiteThreshold;
+
+	public ColourReader() {
+		this(Arrays.asList(HUES), 0.8f, 0.0f, 0.0f);
+	}
+
+	private ColourReader(List<Hue> hues, float resolution, float blackThreshold, float whiteThreshold) {
+		if (hues.isEmpty()) throw new IllegalArgumentException("Empty hues collection not allowed");
+		if (resolution < 0.0) throw new IllegalArgumentException("Resolution value may not be lower than 0.0");
+		if (resolution > 1.0) throw new IllegalArgumentException("Resolution value may not exceed 1.0");
+
+		this.hues = hues;
+		this.resolution = resolution;
+		this.blackThreshold = blackThreshold;
+		this.whiteThreshold = whiteThreshold;
+	}
+
+	public ColourReader withHues(List<Hue> hues) {
+		return new ColourReader(hues, resolution, blackThreshold, whiteThreshold);
+	}
+
+	public ColourReader withResolution(float resolution) {
+		return new ColourReader(hues, resolution, blackThreshold, whiteThreshold);
+	}
+
+	public ColourReader withBlackThreshold(float blackThreshold) {
+		return new ColourReader(hues, resolution, blackThreshold, whiteThreshold);
+	}
+
+	public ColourReader withWhiteThreshold(float whiteThreshold) {
+		return new ColourReader(hues, resolution, blackThreshold, whiteThreshold);
+	}
+
+	public HSBColour averageColour(BufferedImage image) {
 		final List<Integer> samples = readImage(image, resolution);
 
 		final int[] totalRGB = new int[] { 0, 0, 0 };
@@ -28,7 +65,7 @@ public class ColourReader {
 											totalRGB[2] / samples.size(), null));
 	}
 
-	public static List<ColourVolume> colourVolumes(BufferedImage image, float resolution) {
+	public List<ColourVolume> colourVolumes(BufferedImage image) {
 		final List<Integer> samples = readImage(image, resolution);
 
 		final Map<Color, List<HSBColour>> colorList = new HashMap<>();
@@ -37,17 +74,16 @@ public class ColourReader {
 			HSBColour hsb = new HSBColour(Color.RGBtoHSB((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, (rgb) & 0xFF, null));
 			Color color = null;
 
-			// TODO provide way of specifying threshold values for black/white/grey
-
 			// handle black/white/grey separately
-			if (hsb.saturation() == 0f && hsb.brightness() == 1f) {
+			if (hsb.saturation() <= whiteThreshold && hsb.brightness() >= (1f - whiteThreshold)) {
 				color = Color.WHITE;
-			} else if (hsb.hue() == 0f && hsb.saturation() == 0f && hsb.brightness() > 0f) {
+			} else if (hsb.hue() <= blackThreshold && hsb.saturation() <= whiteThreshold
+					   && hsb.brightness() < (1f - whiteThreshold) && hsb.brightness() > blackThreshold) {
 				color = Color.GRAY;
-			} else if (hsb.brightness() == 0f) {
+			} else if (hsb.brightness() <= blackThreshold) {
 				color = Color.BLACK;
 			} else {
-				for (Hue hue : HUES) if (hue.matches(hsb)) color = hue.colors()[0];
+				for (Hue hue : hues) if (hue.matches(hsb)) color = hue.colors()[0];
 			}
 
 			if (!colorList.containsKey(color)) colorList.put(color, new ArrayList<>());
--- test/net/shrimpworks/colours/ColourReaderTest.java
+++ test/net/shrimpworks/colours/ColourReaderTest.java
@@ -3,6 +3,7 @@ package net.shrimpworks.colours;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,19 +15,24 @@ public class ColourReaderTest {
 
 	@Test
 	public void testBadInput() {
-		BufferedImage black = ImageUtils.solidImage(20, 20, Color.BLACK);
-
 		// verify that invalid resolutions are handled as expected
 
 		try {
-			ColourReader.averageColour(black, -0.1f);
+			new ColourReader().withHues(Collections.emptyList());
 			fail("Expected IllegalArgumentException");
 		} catch (IllegalArgumentException expected) {
 			// expected
 		}
 
 		try {
-			ColourReader.averageColour(black, 1.5f);
+			new ColourReader().withResolution(-0.1f);
+			fail("Expected IllegalArgumentException");
+		} catch (IllegalArgumentException expected) {
+			// expected
+		}
+
+		try {
+			new ColourReader().withResolution(1.5f);
 			fail("Expected IllegalArgumentException");
 		} catch (IllegalArgumentException expected) {
 			// expected
@@ -37,26 +43,26 @@ public class ColourReaderTest {
 	public void averageTest() {
 		// verify a solid black image has an average colour of black
 		BufferedImage black = ImageUtils.solidImage(20, 20, Color.BLACK);
-		HSBColour hsb = ColourReader.averageColour(black, 0.5f);
+		HSBColour hsb = new ColourReader().withResolution(0.5f).averageColour(black);
 		assertNotNull(hsb);
 		assertEquals(0, hsb.brightness(), 0.0);
 
 		// verify a solid green image has an average colour of green
 		BufferedImage green = ImageUtils.solidImage(20, 20, Color.GREEN);
-		hsb = ColourReader.averageColour(green, 0.75f);
+		hsb = new ColourReader().averageColour(green);
 		assertNotNull(hsb);
 		assertEquals(120, hsb.hue() * 360, 0.0);
 
 		// verify a solid white image has an average colour of white
 		BufferedImage white = ImageUtils.solidImage(50, 50, Color.WHITE);
-		hsb = ColourReader.averageColour(white, 0.1f);
+		hsb = new ColourReader().withResolution(0.1f).averageColour(white);
 		assertNotNull(hsb);
 		assertEquals(1, hsb.brightness(), 0.0);
 		assertEquals(0, hsb.saturation(), 0.0);
 
 		// verify a solid blue image has an average colour of blue
 		BufferedImage blue = ImageUtils.solidImage(20, 20, Color.BLUE);
-		hsb = ColourReader.averageColour(blue, 0.75f);
+		hsb = new ColourReader().withResolution(0.75f).averageColour(blue);
 		assertNotNull(hsb);
 		assertEquals(240, hsb.hue() * 360, 0.0);
 		assertEquals(1, hsb.brightness(), 0.0);
@@ -67,19 +73,19 @@ public class ColourReaderTest {
 	public void moreAverageTest() {
 		// ger average colour of a 50/50 black/white image, grey result expected
 		BufferedImage grey = ImageUtils.halfHalfImage(20, 20, Color.BLACK, Color.WHITE);
-		HSBColour hsb = ColourReader.averageColour(grey, 1f);
+		HSBColour hsb = new ColourReader().withResolution(1f).averageColour(grey);
 		assertNotNull(hsb);
 		assertEquals(0.5, hsb.brightness(), 0.1);
 
 		// ger average colour of a 50/50 red/green image, yellow result expected
 		BufferedImage yellow = ImageUtils.halfHalfImage(20, 20, Color.RED, Color.GREEN);
-		hsb = ColourReader.averageColour(yellow, 0.5f);
+		hsb = new ColourReader().withResolution(0.5f).averageColour(yellow);
 		assertNotNull(hsb);
 		assertEquals(60, hsb.hue() * 360, 10);
 
 		// ger average colour of a 50/50 red/blue image, magenta result expected
 		BufferedImage pink = ImageUtils.halfHalfImage(20, 20, Color.RED, Color.BLUE);
-		hsb = ColourReader.averageColour(pink, 0.5f);
+		hsb = new ColourReader().averageColour(pink);
 		assertNotNull(hsb);
 		assertEquals(300, hsb.hue() * 360, 10);
 	}
@@ -88,16 +94,18 @@ public class ColourReaderTest {
 	public void colourVolumeTest() {
 		List<ColourVolume> colours;
 
+		ColourReader colourReader = new ColourReader().withResolution(1f);
+
 		// verify getting colours from a solid grey image - 100% grey expected
 		BufferedImage grey = ImageUtils.solidImage(20, 20, Color.GRAY);
-		colours = ColourReader.colourVolumes(grey, 1f);
+		colours = colourReader.colourVolumes(grey);
 		assertNotNull(colours);
 		assertEquals(1, colours.size());
 		assertArrayEquals(getHSB(Color.GRAY), colours.stream().findFirst().get().colour().hsb(), 0.0001f);
 
 		// verify getting colour volumes from a solid red image - 100% full red expected
 		BufferedImage red = ImageUtils.solidImage(20, 20, Color.RED);
-		colours = ColourReader.colourVolumes(red, 1f);
+		colours = colourReader.colourVolumes(red);
 		assertNotNull(colours);
 		assertEquals(1, colours.size());
 		assertArrayEquals(getHSB(Color.RED), colours.stream().findFirst().get().colour().hsb(), 0.0001f);
@@ -105,7 +113,7 @@ public class ColourReaderTest {
 		// get colour volumes from a black and white image - 50/50 split expected
 		List<HSBColour> expected = Arrays.asList(new HSBColour(getHSB(Color.BLACK)), new HSBColour(getHSB(Color.WHITE)));
 		BufferedImage blackAndWhite = ImageUtils.halfHalfImage(20, 20, Color.BLACK, Color.WHITE);
-		colours = ColourReader.colourVolumes(blackAndWhite, 1f);
+		colours = colourReader.colourVolumes(blackAndWhite);
 		assertNotNull(colours);
 		assertEquals(2, colours.size());
 		assertTrue(expected.containsAll(colours.stream().map(ColourVolume::colour).collect(Collectors.toList())));
@@ -113,7 +121,7 @@ public class ColourReaderTest {
 
 		// get colour volumes from a 4-solid-colours image, 4-way 25% split of solid colours expected
 		BufferedImage fourCols = ImageUtils.quartersImage(20, 20, Color.BLACK, Color.WHITE, Color.RED, Color.GREEN);
-		colours = ColourReader.colourVolumes(fourCols, 1f);
+		colours = colourReader.colourVolumes(fourCols);
 		assertNotNull(colours);
 		assertEquals(4, colours.size());
 		expected = Arrays.asList(new HSBColour(getHSB(Color.BLACK)), new HSBColour(getHSB(Color.WHITE)),
@@ -123,7 +131,7 @@ public class ColourReaderTest {
 
 		// get colour volumes from a 3-colour image where one colour is more prevalent, 50/25/25 split of solid colours expected
 		BufferedImage threeCols = ImageUtils.quartersImage(20, 20, Color.BLUE, Color.BLUE, Color.RED, Color.GREEN);
-		colours = ColourReader.colourVolumes(threeCols, 1f);
+		colours = colourReader.colourVolumes(threeCols);
 		assertNotNull(colours);
 		assertEquals(3, colours.size());
 		expected = Arrays.asList(new HSBColour(getHSB(Color.BLUE)), new HSBColour(getHSB(Color.RED)), new HSBColour(getHSB(Color.GREEN)));
